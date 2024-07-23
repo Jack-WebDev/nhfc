81183c16bba9222efc9e0e7cf9f2c8e9af4b4b20
LindaMangena <lindajazz42@gmail.com>
2024-07-23 09:23:13 +0200

    Ui fixes

--- src/app/(web)/frontend/projects/page.tsx
+++ src/app/(web)/frontend/projects/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"; // Ensure this is at the very top of the file
 
 import { Button, Input, Label, PageHeader } from "@/components";
 import {
@@ -10,8 +10,8 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import axios from "axios";
-import { ShoppingBag } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { MapPin, DollarSign, Home } from "lucide-react";
+import { useRouter } from "next/navigation"; // Ensure correct import
 import React, { useEffect, useState } from "react";
 
 type ProjectProps = {
@@ -42,120 +42,210 @@ type ProjectProps = {
 
 export default function Projects() {
   const [projects, setProjects] = useState<ProjectProps[]>([]);
-  const router = useRouter();
+  const [currentPage, setCurrentPage] = useState<number>(1);
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [selectedProvince, setSelectedProvince] = useState<string>("");
 
+  const projectsPerPage = 4;
+  const indexOfLastProject = currentPage * projectsPerPage;
+  const indexOfFirstProject = indexOfLastProject - projectsPerPage;
+  const currentProjects = projects.slice(
+    indexOfFirstProject,
+    indexOfLastProject
+  );
 
-  const filteredProjects = projects.filter((project) =>
-    project.projectName.toLowerCase().includes(searchQuery.toLowerCase()) &&
-    (selectedProvince === "" || project.province === selectedProvince)
+  const filteredProjects = projects.filter(
+    (project) =>
+      project.projectName.toLowerCase().includes(searchQuery.toLowerCase()) &&
+      (selectedProvince === "" || project.province === selectedProvince)
   );
 
   useEffect(() => {
     const fetchProjects = async () => {
       const res = await axios.get("/api/projects");
       setProjects(res.data);
-      console.log(res.data)
     };
 
     fetchProjects();
   }, []);
 
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  if (typeof window === "undefined") {
+    return null; // Return null if the window object is not available (i.e., during SSR)
+  }
+
+  const router = useRouter(); // Initialize the router
+
   return (
     <>
-      <PageHeader Icon={ShoppingBag} title="Projects" />
-      <div className="grid">
-
-        <div className="flex justify-evenly items-center">
-          <div>
-            <label htmlFor="search">Search:</label>
+      <PageHeader Icon={Home} title="Projects" />
+      <div className="grid px-4 py-6 md:px-8 lg:px-12">
+        <div className="flex flex-col md:flex-row justify-between items-center mb-6">
+          <div className="mb-4 md:mb-0 md:mr-4">
+            <label
+              htmlFor="search"
+              className="block mb-2 text-sm font-medium text-gray-700"
+            >
+              Search:
+            </label>
             <input
               type="text"
               placeholder="Search projects"
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
-              className="border p-2 mb-4"
+              className="border p-2 rounded-md w-full md:w-64"
             />
           </div>
 
-          <div>
-        <label htmlFor="province">Province:</label>
-        <select
-          name="province"
-          value={selectedProvince}
-          onChange={(e) => setSelectedProvince(e.target.value)}
-          className="border border-gray-200 rounded-lg p-2 bg-white"
-        >
-          <option value="">Select Province</option>
-          <option value="Gauteng">Gauteng</option>
-          <option value="Western Cape">Western Cape</option>
-          <option value="KwaZulu-Natal">KwaZulu-Natal</option>
-          <option value="Eastern Cape">Eastern Cape</option>
-          <option value="Free State">Free State</option>
-          <option value="Limpopo">Limpopo</option>
-          <option value="Mpumalanga">Mpumalanga</option>
-          <option value="North West">North West</option>
-          <option value="Northern Cape">Northern Cape</option>
-        </select>
-      </div>
-          <div>
-            <label htmlFor="projectName">Project Name:</label>
-            <select
-              name="projectName"
-              // value={formData.projectName || ""}
-              // onChange={handleChange}
-              className="border border-gray-200 rounded-lg p-2 bg-white"
+          <div className="mb-4 md:mb-0">
+            <label
+              htmlFor="province"
+              className="block mb-2 text-sm font-medium text-gray-700"
             >
-              <option value="" disabled>
-                Select Project
-              </option>
-              <option value="Fleurhof Integrated Housing Development">
-                Fleurhof Integrated Housing Development
-              </option>
-              <option value="Belhar Social Housing Project">
-                Belhar Social Housing Project
-              </option>
-              <option value="Westgate Social Housing Project">
-                Westgate Social Housing Project
-              </option>
-              <option value="Devland Gardens">Devland Gardens</option>
-              <option value="Southernwood Square">Southernwood Square</option>
-              <option value="Thembelihle Village">Thembelihle Village</option>
+              Province:
+            </label>
+            <select
+              name="province"
+              value={selectedProvince}
+              onChange={(e) => setSelectedProvince(e.target.value)}
+              className="border p-2 rounded-md w-full md:w-64"
+            >
+              <option value="">Select Province</option>
+              <option value="Gauteng">Gauteng</option>
+              <option value="Western Cape">Western Cape</option>
+              <option value="KwaZulu-Natal">KwaZulu-Natal</option>
+              <option value="Eastern Cape">Eastern Cape</option>
+              <option value="Free State">Free State</option>
+              <option value="Limpopo">Limpopo</option>
+              <option value="Mpumalanga">Mpumalanga</option>
+              <option value="North West">North West</option>
+              <option value="Northern Cape">Northern Cape</option>
             </select>
           </div>
         </div>
-      <div className="flex flex-wrap justify-center gap-8">
 
-        {filteredProjects.map((project) => (
-          <Card className="w-[350px] mt-12" key={project.id}>
-            <CardHeader>
-              <CardTitle>{project.projectName}</CardTitle>
-              <CardDescription>{project.deliverablesSummary}</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <ul>
-                <li>Location: Johannesburg, {project.province}</li>
-                <li>Units: 10,000+</li>
-
-                <div>
-                  <h2>Project Values: R3.5 Billion</h2>
-                </div>
-              </ul>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-              <Button
-                variant="default"
-                className="bg-blue-500 text-white hover:bg-blue-700"
-                onClick={() => router.push(`/frontend/projects/${project.id}`)}
+        <div className="flex flex-wrap justify-center gap-8">
+          {filteredProjects
+            .slice(indexOfFirstProject, indexOfLastProject)
+            .map((project) => (
+              <Card
+                className="w-[350px] p-6 bg-white shadow-lg rounded-lg flex flex-col justify-between hover:shadow-xl transition-shadow duration-300 hover:transform hover:scale-105 border border-gray-200"
+                key={project.id}
               >
-                View Details
-              </Button>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
+                <div>
+                  <CardHeader>
+                    <CardTitle className="text-lg font-semibold">
+                      {project.projectName}
+                    </CardTitle>
+                    <CardDescription className="text-gray-500">
+                      {project.deliverablesSummary}
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent className="mt-4">
+                    <ul className="space-y-2">
+                      <li className="flex items-center">
+                        <MapPin className="w-5 h-5 mr-2 text-blue-500" />
+                        <span className="text-sm text-gray-600">
+                          Location: {project.address}, {project.province}
+                        </span>
+                      </li>
+                      <li className="flex items-center">
+                        <Home className="w-5 h-5 mr-2 text-green-500" />
+                        <span className="text-sm text-gray-600">
+                          Units: 10,000+
+                        </span>
+                      </li>
+                      <li className="flex items-center">
+                        <DollarSign className="w-5 h-5 mr-2 text-yellow-500" />
+                        <span className="text-sm text-gray-600">
+                          Project Value: R3.5 Billion
+                        </span>
+                      </li>
+                    </ul>
+                  </CardContent>
+                </div>
+                <CardFooter className="mt-4">
+                  <Button
+                    variant="default"
+                    className="w-full bg-blue-500 text-white hover:bg-blue-700"
+                    onClick={() =>
+                      router.push(`/frontend/projects/${project.id}`)
+                    } // Use router.push for navigation
+                  >
+                    View Details
+                  </Button>
+                </CardFooter>
+              </Card>
+            ))}
+        </div>
+
+        <Pagination
+          projectsPerPage={projectsPerPage}
+          totalProjects={filteredProjects.length}
+          paginate={paginate}
+          currentPage={currentPage}
+        />
       </div>
     </>
   );
 }
+
+const Pagination = ({
+  projectsPerPage,
+  totalProjects,
+  paginate,
+  currentPage,
+}: {
+  projectsPerPage: number;
+  totalProjects: number;
+  paginate: (pageNumber: number) => void;
+  currentPage: number;
+}) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalProjects / projectsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav className="mt-4">
+      <ul className="flex justify-center space-x-2">
+        {currentPage > 1 && (
+          <li>
+            <button
+              onClick={() => paginate(currentPage - 1)}
+              className="px-3 py-1 rounded-md bg-blue-500 text-white hover:bg-blue-700"
+            >
+              Previous
+            </button>
+          </li>
+        )}
+        {pageNumbers.map((number) => (
+          <li key={number}>
+            <button
+              onClick={() => paginate(number)}
+              className={`px-3 py-1 rounded-md ${
+                currentPage === number
+                  ? "bg-blue-700 text-white"
+                  : "bg-blue-500 text-white hover:bg-blue-700"
+              }`}
+            >
+              {number}
+            </button>
+          </li>
+        ))}
+        {currentPage < pageNumbers.length && (
+          <li>
+            <button
+              onClick={() => paginate(currentPage + 1)}
+              className="px-3 py-1 rounded-md bg-blue-500 text-white hover:bg-blue-700"
+            >
+              Next
+            </button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
